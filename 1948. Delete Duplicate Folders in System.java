22934bd53a8bfbfc087268e4463d533f600bbe81
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-07-20 12:59:54 +0530

    Create 1948. Delete Duplicate Folders in System.java
    Due to a bug, there are many duplicate folders in a file system. You are given a 2D array paths, where paths[i] is an array representing an absolute path to the ith folder in the file system.
    
    For example, ["one", "two", "three"] represents the path "/one/two/three".
    Two folders (not necessarily on the same level) are identical if they contain the same non-empty set of identical subfolders and underlying subfolder structure. The folders do not need to be at the root level to be identical. If two or more folders are identical, then mark the folders as well as all their subfolders.
    
    For example, folders "/a" and "/b" in the file structure below are identical. They (as well as their subfolders) should all be marked:
    /a
    /a/x
    /a/x/y
    /a/z
    /b
    /b/x
    /b/x/y
    /b/z
    However, if the file structure also included the path "/b/w", then the folders "/a" and "/b" would not be identical. Note that "/a/x" and "/b/x" would still be considered identical even with the added folder.
    Once all the identical folders and their subfolders have been marked, the file system will delete all of them. The file system only runs the deletion once, so any folders that become identical after the initial deletion are not deleted.
    
    Return the 2D array ans containing the paths of the remaining folders after deleting all the marked folders. The paths may be returned in any order.
    
     
    
    Example 1:
    https://assets.leetcode.com/uploads/2021/07/19/lc-dupfolder1.jpg
    Input: paths = [["a"],["c"],["d"],["a","b"],["c","b"],["d","a"]]
    Output: [["d"],["d","a"]]
    Explanation: The file structure is as shown.
    Folders "/a" and "/c" (and their subfolders) are marked for deletion because they both contain an empty
    folder named "b".
    Example 2:
    https://assets.leetcode.com/uploads/2021/07/19/lc-dupfolder2.jpg
    Input: paths = [["a"],["c"],["a","b"],["c","b"],["a","b","x"],["a","b","x","y"],["w"],["w","y"]]
    Output: [["c"],["c","b"],["a"],["a","b"]]
    Explanation: The file structure is as shown. 
    Folders "/a/b/x" and "/w" (and their subfolders) are marked for deletion because they both contain an empty folder named "y".
    Note that folders "/a" and "/c" are identical after the deletion, but they are not deleted because they were not marked beforehand.
    Example 3:

---
+++ 1948. Delete Duplicate Folders in System.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> deleteDuplicateFolder(List<List<String>> paths) {
+        TrieNode root = new TrieNode();
+        for (List<String> path : paths) {
+            TrieNode curr = root;
+            for (String folder : path) {
+                if (!curr.children.containsKey(folder)) {
+                    curr.children.put(folder, new TrieNode());
+                }
+                curr = curr.children.get(folder);
+            }
+        }
+
+        Map<String, TrieNode> seen = new HashMap<>();
+        buildHash(root, seen);
+
+        List<List<String>> result = new ArrayList<>();
+        dfs(root, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private String buildHash(TrieNode node, Map<String, TrieNode> seen) {
+        StringBuilder sb = new StringBuilder();
+        List<String> keys = new ArrayList<>(node.children.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            TrieNode child = node.children.get(key);
+            sb.append(key).append(buildHash(child, seen)).append("|");
+        }
+        String hash = sb.toString();
+        if (!hash.isEmpty()) {
+            if (seen.containsKey(hash)) {
+                seen.get(hash).marked = true;
+                node.marked = true;
+            } else {
+                seen.put(hash, node);
+            }
+        }
+        return hash;
+    }
+
+    private void dfs(TrieNode node, List<String> path, List<List<String>> result) {
+        for (Map.Entry<String, TrieNode> entry : node.children.entrySet()) {
+            String folder = entry.getKey();
+            TrieNode child = entry.getValue();
+            if (!child.marked) {
+                List<String> newPath = new ArrayList<>(path);
+                newPath.add(folder);
+                result.add(newPath);
+                dfs(child, newPath, result);
+            }
+        }
+    }
+
+    static class TrieNode {
+        Map<String, TrieNode> children;
+        boolean marked;
+
+        TrieNode() {
+            children = new HashMap<>();
+            marked = false;
+        }
+    }
+}
