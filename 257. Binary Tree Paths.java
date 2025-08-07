471760f5b406bc4481c963371a768b39475a00f4
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-07 11:13:57 +0530

    Create 257. Binary Tree Paths.java
    Given the root of a binary tree, return all root-to-leaf paths in any order.
    
    A leaf is a node with no children.
    
     
    
    Example 1:
    https://assets.leetcode.com/uploads/2021/03/12/paths-tree.jpg
    Input: root = [1,2,3,null,5]
    Output: ["1->2->5","1->3"]
    Example 2:
    
    Input: root = [1]
    Output: ["1"]
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 100].
    -100 <= Node.val <= 100

---
+++ 257. Binary Tree Paths.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+        dfs(root, "", paths);
+        return paths;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> paths) {
+        if (node == null) return;
+        path += node.val;
+        if (node.left == null && node.right == null) {
+            paths.add(path);
+        } else {
+            path += "->";
+            dfs(node.left, path, paths);
+            dfs(node.right, path, paths);
+        }
+    }
+}
