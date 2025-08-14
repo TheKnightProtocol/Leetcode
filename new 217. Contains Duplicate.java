21aaa207a601101ddc7c1e6109812bba915654e6
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-14 20:41:38 +0530

    Create new 217. Contains Duplicate.java

---
+++ new 217. Contains Duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) return true;
+        }
+        return false;
+    }
+}
