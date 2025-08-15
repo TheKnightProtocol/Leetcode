4f9dec4466d37798eab9860f824277e41ca4652d
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-15 11:32:43 +0530

    Create 107. Binary Tree Level Order Traversal II.java
    Given the root of a binary tree, return the bottom-up level order traversal of its nodes' values. (i.e., from left to right, level by level from leaf to root).
    
     
    
    Example 1:
    https://assets.leetcode.com/uploads/2021/02/19/tree1.jpg
    Input: root = [3,9,20,null,null,15,7]
    Output: [[15,7],[9,20],[3]]
    Example 2:
    
    Input: root = [1]
    Output: [[1]]
    Example 3:
    
    Input: root = []
    Output: []
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [0, 2000].
    -1000 <= Node.val <= 1000

---
+++ 107. Binary Tree Level Order Traversal II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            res.add(0, level);
+        }
+        return res;
+    }
+}
