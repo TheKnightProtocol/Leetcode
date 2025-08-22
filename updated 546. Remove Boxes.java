5515e894b85cd5d8de761160d173e02afabc8695
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-22 16:46:19 +0530

    Create updated 546. Remove Boxes.java
    You are given several boxes with different colors represented by different positive numbers.
    
    You may experience several rounds to remove boxes until there is no box left. Each time you can choose some continuous boxes with the same color (i.e., composed of k boxes, k >= 1), remove them and get k * k points.
    
    Return the maximum points you can get.
    
     
    
    Example 1:
    
    Input: boxes = [1,3,2,2,2,3,4,3,1]
    Output: 23
    Explanation:
    [1, 3, 2, 2, 2, 3, 4, 3, 1] 
    ----> [1, 3, 3, 4, 3, 1] (3*3=9 points) 
    ----> [1, 3, 3, 3, 1] (1*1=1 points) 
    ----> [1, 1] (3*3=9 points) 
    ----> [] (2*2=4 points)
    Example 2:
    
    Input: boxes = [1,1,1]
    Output: 9
    Example 3:
    
    Input: boxes = [1]
    Output: 1
     
    
    Constraints:
    
    1 <= boxes.length <= 100
    1 <= boxes[i] <= 100

---
+++ updated 546. Remove Boxes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int removeBoxes(int[] boxes) {
+        int n = boxes.length;
+        int[][][] dp = new int[n][n][n];
+        return dfs(boxes, dp, 0, n - 1, 0);
+    }
+
+    private int dfs(int[] boxes, int[][][] dp, int l, int r, int k) {
+        if (l > r) return 0;
+        if (dp[l][r][k] != 0) return dp[l][r][k];
+        while (r > l && boxes[r] == boxes[r - 1]) {
+            r--;
+            k++;
+        }
+        dp[l][r][k] = dfs(boxes, dp, l, r - 1, 0) + (k + 1) * (k + 1);
+        for (int i = l; i < r; i++) {
+            if (boxes[i] == boxes[r]) {
+                dp[l][r][k] = Math.max(dp[l][r][k],
+                    dfs(boxes, dp, l, i, k + 1) + dfs(boxes, dp, i + 1, r - 1, 0));
+            }
+        }
+        return dp[l][r][k];
+    }
+}
