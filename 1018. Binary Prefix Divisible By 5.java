3344fc34e0d8849cfc4371495f443e52aea439a6
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2025-11-24 20:58:40 +0530

    Create 1018. Binary Prefix Divisible By 5.java
    You are given a binary array nums (0-indexed).
    
    We define xi as the number whose binary representation is the subarray nums[0..i] (from most-significant-bit to least-significant-bit).
    
    For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
    Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
    
     
    
    Example 1:
    
    Input: nums = [0,1,1]
    Output: [true,false,false]
    Explanation: The input numbers in binary are 0, 01, 011; which are 0, 1, and 3 in base-10.
    Only the first number is divisible by 5, so answer[0] is true.
    Example 2:
    
    Input: nums = [1,1,1]
    Output: [false,false,false]
     
    
    Constraints:
    
    1 <= nums.length <= 105
    nums[i] is either 0 or 1.

---
+++ 1018. Binary Prefix Divisible By 5.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> result = new ArrayList<>();
+        int num = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            num = (num * 2 + nums[i]) % 5;
+            result.add(num == 0);
+        }
+        
+        return result;
+    }
+}
