36664615b4af63c1782e6bfa3d1bb0fb7ffc407f
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-01-06 20:30:27 +0530

    Create 1161. Maximum Level Sum of a Binary Tree.cpp
    Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
    
    Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
    
     
    
    Example 1:
    https://assets.leetcode.com/uploads/2019/05/03/capture.JPG
    
    Input: root = [1,7,0,7,-8,null,null]
    Output: 2
    Explanation: 
    Level 1 sum = 1.
    Level 2 sum = 7 + 0 = 7.
    Level 3 sum = 7 + -8 = -1.
    So we return the level with the maximum sum which is level 2.
    Example 2:
    
    Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
    Output: 2
     
    
    Constraints:
    
    The number of nodes in the tree is in the range [1, 104].
    -105 <= Node.val <= 105

---
+++ 1161. Maximum Level Sum of a Binary Tree.cpp
@@ -0,0 +1,40 @@
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if (!root) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int maxSum = INT_MIN;
+        int resultLevel = 0;
+        int currentLevel = 0;
+        
+        while (!q.empty()) {
+            currentLevel++;
+            int levelSize = q.size();
+            int levelSum = 0;
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                levelSum += node->val;
+                
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                resultLevel = currentLevel;
+            }
+        }
+        
+        return resultLevel;
+    }
+};
