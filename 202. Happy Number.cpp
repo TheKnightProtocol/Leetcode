3e9a9d78c4815aa55079df8159f612fff385b1aa
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-04-17 17:10:54 +0530

    Create 202. Happy Number.cpp
    Write an algorithm to determine if a number n is happy.
    
    A happy number is a number defined by the following process:
    
    Starting with any positive integer, replace the number by the sum of the squares of its digits.
    Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
    Those numbers for which this process ends in 1 are happy.
    Return true if n is a happy number, and false if not.
    
     
    
    Example 1:
    
    Input: n = 19
    Output: true
    Explanation:
    12 + 92 = 82
    82 + 22 = 68
    62 + 82 = 100
    12 + 02 + 02 = 1
    Example 2:
    
    Input: n = 2
    Output: false
     
    
    Constraints:
    
    1 <= n <= 231 - 1

---
+++ 202. Happy Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = sumOfSquares(n);
+        
+        while (fast != 1 && slow != fast) {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+        }
+        
+        return fast == 1;
+    }
+    
+private:
+    int sumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+};
