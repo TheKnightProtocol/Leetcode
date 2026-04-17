6b0789c8828e36baaa456e52ccff47ae0e0b3e31
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-04-17 19:11:49 +0530

    Add solution for populating next right pointers
    You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
    
    struct Node {
      int val;
      Node *left;
      Node *right;
      Node *next;
    }
    Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
    
    Initially, all next pointers are set to NULL.
    
     
    
    Example 1:
    https://assets.leetcode.com/uploads/2019/02/14/116_sample.png

---
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+        
+        Node* levelStart = root;
+        
+        while (levelStart->left) {
+            Node* curr = levelStart;
+            
+            while (curr) {
+                curr->left->next = curr->right;
+                
+                if (curr->next) {
+                    curr->right->next = curr->next->left;
+                }
+                
+                curr = curr->next;
+            }
+            
+            levelStart = levelStart->left;
+        }
+        
+        return root;
+    }
+};
