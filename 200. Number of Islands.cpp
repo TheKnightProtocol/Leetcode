11ee2b55fa3eb17ef671a6786b4f6ba842702e4f
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-04-17 16:50:36 +0530

    Create 200. Number of Islands.cpp
    Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
    
    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
    
     
    
    Example 1:
    
    Input: grid = [
      ["1","1","1","1","0"],
      ["1","1","0","1","0"],
      ["1","1","0","0","0"],
      ["0","0","0","0","0"]
    ]
    Output: 1
    Example 2:
    
    Input: grid = [
      ["1","1","0","0","0"],
      ["1","1","0","0","0"],
      ["0","0","1","0","0"],
      ["0","0","0","1","1"]
    ]
    Output: 3
     
    
    Constraints:
    
    m == grid.length
    n == grid[i].length
    1 <= m, n <= 300
    grid[i][j] is '0' or '1'.

---
+++ 200. Number of Islands.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty() || grid[0].empty()) return 0;
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0;
+        
+        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    islands++;
+                    
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    grid[i][j] = '0';
+                    
+                    while (!q.empty()) {
+                        auto [x, y] = q.front();
+                        q.pop();
+                        
+                        for (auto& dir : dirs) {
+                            int nx = x + dir[0];
+                            int ny = y + dir[1];
+                            
+                            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] == '1') {
+                                grid[nx][ny] = '0';
+                                q.push({nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return islands;
+    }
+};
