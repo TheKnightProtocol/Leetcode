5ce96f7b12153b88a51ada945a62e2aeca7a08e6
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-04-22 20:52:15 +0530

    Create 179. Largest Number.java
    Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
    
    Since the result may be very large, so you need to return a string instead of an integer.
    
     
    
    Example 1:
    
    Input: nums = [10,2]
    Output: "210"
    Example 2:
    
    Input: nums = [3,30,34,5,9]
    Output: "9534330"
     
    
    Constraints:
    
    1 <= nums.length <= 100
    0 <= nums[i] <= 109

---
+++ 179. Largest Number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strs, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+
+        if (strs[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
