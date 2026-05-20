6699638b80240ac500528b8e81cb3e742bf5471e
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-05-20 15:42:06 +0530

    Add solution for finding prefix common array
    Implement a solution to find the prefix common array of two input arrays.

---
+++ 2657. Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<bool> seenA(n + 1, false), seenB(n + 1, false);
+        vector<int> C(n);
+        int common = 0;
+        for (int i = 0; i < n; ++i) {
+            seenA[A[i]] = true;
+            if (seenB[A[i]]) ++common;
+            seenB[B[i]] = true;
+            if (seenA[B[i]]) ++common;
+            C[i] = common;
+        }
+        return C;
+    }
+};
