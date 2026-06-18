3837a85cc931780c8d033740b119c6cf31eca20d
Sankalp <160457369+TheKnightProtocol@users.noreply.github.com>
2026-06-18 11:32:52 +0530

    Create 1344. Angle Between Hands of a Clock.java
    Given two numbers, hour and minutes, return the smaller angle (in degrees) formed between the hour and the minute hand.
    
    Answers within 10-5 of the actual value will be accepted as correct.
    
     
    
    Example 1:
    
    https://assets.leetcode.com/uploads/2019/12/26/sample_1_1673.png
    
    Input: hour = 12, minutes = 30
    Output: 165
    Example 2:
    
    https://assets.leetcode.com/uploads/2019/12/26/sample_2_1673.png
    
    Input: hour = 3, minutes = 30
    Output: 75
    Example 3:
    
    
    
    https://assets.leetcode.com/uploads/2019/12/26/sample_3_1673.png
    
    Input: hour = 3, minutes = 15
    Output: 7.5
     
    
    Constraints:
    
    1 <= hour <= 12
    0 <= minutes <= 59

---
+++ 1344. Angle Between Hands of a Clock.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double hourAngle = (hour % 12) * 30.
+        0 + minutes * 0.5;
+        double minAngle = minutes * 6.0;
+        double diff = Math.abs(hourAngle - minAngle);
+        return Math.min(diff, 360.0 - diff);
+    }
+}
